d78c7b6eb1fd12e294378afda2cc6302c93b6adf
Dennis-Kipkogei <kipkogeidennis05@gmail.com>
2025-09-26 16:39:58 +0300

    Remove unnecessary files and clean up project structure

--- server/app.py
+++ server/app.py
@@ -8,14 +8,11 @@ from flask_restful import Resource
 
 # Local imports
 from config import app, db
-from routes.user_routes import user_bp
 from routes.order_routes import order_bp
 from routes.menu_routes import menu_bp
 from routes.category_routes import category_bp
 from routes.admin_routes import admin_bp
 from routes.auth_routes import auth_bp
-
-app.register_blueprint(user_bp)
 app.register_blueprint(order_bp)
 app.register_blueprint(menu_bp)
 app.register_blueprint(category_bp)
